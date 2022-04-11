cbe3cf178cdc2a9abe4f2300262b0e8e510bc318
sumit-jha94 <sumitshine14@gmail.com>
2022-04-11 14:58:52 +0530

    pagination page dynamically active

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-export default function Home() {
+let pageSize = 10;
+export default function HomeDummy() {
   const [data, setData] = useState(null);
+  const [posts, setPosts] = useState();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
-  // const onLinkClick = = () => {
-  //   window.open("http://twitter.com/saigowthamr");
-  // };
+  const [input, setInput] = useState('');
 
   useEffect(() => {
     fetchUser(1);
   }, []);
 
+  const pageCount = posts ? Math.ceil(posts.length/pageSize):0;
+  if (pageCount === 1) return null;
+
   async function fetchUser(check=2) {
+      setLoading(true)
     if(check === 1 ){
       await fetch(`https://datapeace-storage.s3-us-west-2.amazonaws.com/dummy_data/users.json?_limit=20`)
       .then((response) => {
@@ -27,6 +30,7 @@ export default function Home() {
       })
       .then((actualData) => {
         setData(actualData);
+        setPosts(actualData);
         setError(null);
       })
       .catch((err) => {
@@ -39,14 +43,11 @@ export default function Home() {
     }
   }
 
-  const [input, setInput] = useState('');
 
   const onInputChange = (ev) => {
     setInput(ev.target.value);
-    console.log(ev.target.value)
     searchUser(ev.target.value)
   }
-
 
   const onButton = (ev) => {
     setInput(ev.target.value);
@@ -54,7 +55,7 @@ export default function Home() {
   }
 
   async function searchUser(search){
-    console.log(search+'rrrrrrrrrrrrr')
+    
     if(search != ''){
       setData(null)
       await fetchUser(1);
@@ -69,8 +70,8 @@ export default function Home() {
   }
 
   return (
-    <div className="App">
-      <div>
+    <div>
+      <div className="float-xl-right">
         <input type="text"
         placeholder='Type Query'
         name="search"
@@ -79,72 +80,62 @@ export default function Home() {
         <button type='button' onClick={onButton}> Search</button>
       </div>
 
-      <h1>Data Sheet</h1>
+      <h1 className="text-center text-primary text-uppercase">Data Sheet</h1>
 
       {loading && <div>A moment please...</div>}
       {error && (
         <div>{`There is a problem fetching the post data - ${error}`}</div>
       )}
-      <table border="2" cellpadding="30" responsive="md">
-
-    
-
-      <td> <h3>Sr. No. </h3>
-              {data &&
-                data.map(({ id, first_name, last_name, email, web, age }) => (
-                    <ol key={id}> {id}</ol>
-               ))}
-               </td>
-
-
-         {/* First Name Table */}
-        <td> <h3>First Name </h3>
-
-              {data &&
-                data.map(({ id, first_name, last_name, email, web, age }) => (
-                  <Link to={`/user/${id} `}>
-                    <p key={id}> {first_name}</p>
-                    </Link>
-               ))}
-
-               </td>
-
-
-          {/* Last Name Table */}
-        <td > <h3>Last Name</h3>
-              {data &&
-                data.map(({ id, first_name, last_name, email, web, age }) => (
-                    <p key={id}>{last_name}</p>
-               ))}
-               </td>
-
-                {/* email Table */}
-        <td> <h3>Email Address</h3>
-              {data &&
-                data.map(({ id, first_name, last_name, email, web, age }) => (
-                    <p key={id}>{email}</p>
-               ))}
-               </td>
-
-               {/* web Table */}
-        <td> <h3>Website</h3>
-              {data &&
-                data.map(({ id, first_name, last_name, email, web, age }) => (
-                    <p key={id}>{web}</p>
-               ))}
-               </td>
-
-               {/* age Table */}
-                      <td> <h3>Age</h3>
-                        {data &&
-                          data.map(({ id, first_name, last_name, email, web, age }) => (
-                              <p key={id}>{age}</p>
-                      ))}
-                      </td>
+      <table className='table table-hover table-responsive table-bordered' >
+            <thead>
+                <tr className='table-active'>
+                    <th className='table-primary'> Sr. No.</th>
+                    <th className='table-secondary'>First Name </th>
+                    <th className='table-success'>Last Name</th>
+                    <th className='table-danger'>Email Address</th>
+                    <th className='table-warning'>Website</th>
+                    <th className='table-info'>Age</th>
+                </tr>
+            </thead>
+            <tbody>
+                { 
+                data &&
+                data.map(({id, first_name, last_name, email, web, age})=> (
+                        <tr key={id}>
+                            <td className='table-primary'>{id}</td>
+                            <td className='table-secondary'>
+                            <Link to={`/user/${id} `}>
+                            {first_name} </Link>
+                            </td>
+                            <td className='table-success'>{last_name}</td>
+                            <td className='table-danger'>{email}</td>
+                            <td className='table-warning'>{web}</td>
+                            <td className='table-info'>{age}</td>
+                        </tr>
+                    ))}
+                
+            </tbody>
+        </table>
 
 
 
-             </table>
+
+        <nav className="Page navigation example">
+          <ul className="pagination justify-content-center">
+            <ol className="page-item disabled page-link ">1</ol>
+            <ol className="page-item disabled page-link ">1</ol>
+            <ol className="page-item disabled page-link ">1</ol>
+            
+          </ul>
+        </nav>
+
+
+
+
+
+
+
+
     </div>
   );
 }
--- src/components/pages/HomeDummy.js
+++ src/components/pages/HomeDummy.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import _ from "lodash";
 
+let pageSize = 50;
 export default function HomeDummy() {
   const [data, setData] = useState(null);
+  const [paginatedPost, setPaginatedPost] = useState();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [input, setInput] = useState('');
 
   useEffect(() => {
     fetchUser(1);
   }, []);
 
+  
   async function fetchUser(check=2) {
       setLoading(true)
     if(check === 1 ){
@@ -24,6 +29,7 @@ export default function HomeDummy() {
       })
       .then((actualData) => {
         setData(actualData);
+        
         setError(null);
       })
       .catch((err) => {
@@ -36,11 +42,9 @@ export default function HomeDummy() {
     }
   }
 
-  const [input, setInput] = useState('');
 
   const onInputChange = (ev) => {
     setInput(ev.target.value);
-    console.log(ev.target.value)
     searchUser(ev.target.value)
   }
 
@@ -50,7 +54,7 @@ export default function HomeDummy() {
   }
 
   async function searchUser(search){
-    console.log(search+'rrrrrrrrrrrrr')
+    
     if(search != ''){
       setData(null)
       await fetchUser(1);
@@ -64,9 +68,14 @@ export default function HomeDummy() {
     }
   }
 
+  const pageCount = data ? Math.ceil(data.length/pageSize):0;
+  if (pageCount === 1) return null;
+
+  const pages = _.range(1, pageCount+1);
+
   return (
-    <div className="App">
-      <div>
+    <div>
+      <div className="float-xl-right">
         <input type="text"
         placeholder='Type Query'
         name="search"
@@ -75,42 +84,58 @@ export default function HomeDummy() {
         <button type='button' onClick={onButton}> Search</button>
       </div>
 
-      <h1>Data Sheet</h1>
+      <h1 className="text-center text-primary text-uppercase">Data Sheet</h1>
 
       {loading && <div>A moment please...</div>}
       {error && (
         <div>{`There is a problem fetching the post data - ${error}`}</div>
       )}
-      <table className='table table-hover table-responsive' >
+      <table className='table table-hover table-responsive table-bordered' >
             <thead>
                 <tr className='table-active'>
                     <th className='table-primary'> Sr. No.</th>
                     <th className='table-secondary'>First Name </th>
                     <th className='table-success'>Last Name</th>
                     <th className='table-danger'>Email Address</th>
-                    <th className='table-danger'>Website</th>
-                    <th className='table-danger'>Age</th>
+                    <th className='table-warning'>Website</th>
+                    <th className='table-info'>Age</th>
                 </tr>
             </thead>
             <tbody>
-                { data &&
-                    data.map(({post, id, first_name, last_name, email, web, age, index})=> (
+                { 
+                data &&
+                data.map(({id, first_name, last_name, email, web, age})=> (
                         <tr key={id}>
                             <td className='table-primary'>{id}</td>
-                            
                             <td className='table-secondary'>
                             <Link to={`/user/${id} `}>
                             {first_name} </Link>
                             </td>
                             <td className='table-success'>{last_name}</td>
-                            <td className='table-success'>{email}</td>
-                            <td className='table-success'>{web}</td>
-                            <td className='table-success'>{age}</td>
+                            <td className='table-danger'>{email}</td>
+                            <td className='table-warning'>{web}</td>
+                            <td className='table-info'>{age}</td>
                         </tr>
                     ))}
                 
             </tbody>
         </table>
+
+        <nav className="Page navigation example">
+          <ul className="pagination justify-content-center">
+
+                  {
+                    pages.map((data) =>(
+                      <li className="page-item disabled page-link ">{data}</li>
+                    ))
+                  }
+
+            
+            
+            
+          </ul>
+        </nav>
+
     </div>
   );
 }
